e1cd4e4776a5aaeb7b1d9996ed776d4d970cc6e3
Peter O'Brien <peter.obrien1@rsa.com>
2018-05-22 12:03:37 -0400

    Adding UserEntity and H2 console

--- src/main/java/com/rsa/demo/Entities/UserEntity.java
+++ src/main/java/com/rsa/demo/Entities/UserEntity.java
@@ -1,4 +1,31 @@
 package com.rsa.demo.Entities;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
 public class UserEntity {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String name;
+
+    @Column
+    private Integer age;
+
+    @Column
+    private String email;
+
 }
