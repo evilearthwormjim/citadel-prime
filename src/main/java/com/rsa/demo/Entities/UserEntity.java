f05e5977b154ee29f600715a7bb94cd9b0d5402a
Peter O'Brien <peter.obrien1@rsa.com>
2018-05-22 13:33:52 -0400

    Adding user getter and setters

--- src/main/java/com/rsa/demo/Entities/UserEntity.java
+++ src/main/java/com/rsa/demo/Entities/UserEntity.java
@@ -28,4 +28,52 @@ public class UserEntity {
     @Column
     private String email;
 
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
