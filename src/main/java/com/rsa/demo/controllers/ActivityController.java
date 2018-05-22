619daf7d13ca49a14294669aa71e18dedfb34818
Reuben Fisher <reuben.fisher@rsa.com>
2018-05-22 16:19:39 -0400

    fix compile error

--- src/main/java/com/rsa/demo/controllers/ActivityController.java
+++ src/main/java/com/rsa/demo/controllers/ActivityController.java
@@ -1,6 +1,7 @@
 package com.rsa.demo.controllers;
 
 import com.rsa.demo.models.ActivityModel;
+import com.rsa.demo.services.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +15,7 @@ import javax.validation.Valid;
 public class ActivityController {
 
     @Autowired
-    com.rsa.demo.Services.ActivityService service;
+    ActivityService service;
 
     @PostMapping
     ActivityModel create(@Valid @RequestBody ActivityModel activityMdl){
