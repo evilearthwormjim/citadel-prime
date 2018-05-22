eff1d25f6e0556f2171e20fa71575f55ef7fa6a4
kulkav9 <vaishali.kulkarni@rsa.com>
2018-05-22 14:19:00 -0400

    Adding Event Table

--- src/main/java/com/rsa/demo/Entities/Event.java
+++ src/main/java/com/rsa/demo/Entities/Event.java
@@ -1,4 +1,78 @@
 package com.rsa.demo.Entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 public class Event {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
+    private String Name;
+
+    @Column
+    private String activityId;
+
+    @Column
+    private Date scheduledDate;
+
+    @Column
+    private boolean isActive;
+
+    @Column
+    private long ownerId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(String activityId) {
+        this.activityId = activityId;
+    }
+
+    public Date getScheduledDate() {
+        return scheduledDate;
+    }
+
+    public void setScheduledDate(Date scheduledDate) {
+        this.scheduledDate = scheduledDate;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(long ownerId) {
+        this.ownerId = ownerId;
+    }
 }
