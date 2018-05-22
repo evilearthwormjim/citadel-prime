ea758df22ba97f628cb8df09c5f3498b31b1c537
Peter O'Brien <peter.obrien1@rsa.com>
2018-05-22 15:54:57 -0400

    Save user controller/service added

--- src/main/java/com/rsa/demo/services/UserService.java
+++ src/main/java/com/rsa/demo/services/UserService.java
@@ -3,6 +3,7 @@ package com.rsa.demo.services;
 import com.rsa.demo.entities.UserEntity;
 import com.rsa.demo.models.UserModel;
 import com.rsa.demo.repositories.UserRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,9 @@ public class UserService {
     public void saveUser(UserModel user){
 
         UserEntity newUser = new UserEntity();
+        BeanUtils.copyProperties(user, newUser, "id");
         userRepository.save(newUser);
 
+        user.setId(newUser.getId());
     }
 }
