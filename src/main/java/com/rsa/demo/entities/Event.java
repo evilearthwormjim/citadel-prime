742dfa82eb680f4a3be36003a9195140a6ef6894
kulkav9 <vaishali.kulkarni@rsa.com>
2018-05-22 16:35:52 -0400

    Merge branch 'master' of https://github.com/evilearthwormjim/citadel-prime
    # Conflicts:
    #	src/main/java/com/rsa/demo/entities/Event.java

--- src/main/java/com/rsa/demo/entities/Event.java
+++ src/main/java/com/rsa/demo/entities/Event.java
@@ -1,5 +1,7 @@
 package com.rsa.demo.entities;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import javax.persistence.Id;
 import java.util.Date;
 
 @Entity
+@Data
 public class Event {
 
     @Id
@@ -27,52 +30,4 @@ public class Event {
 
     @Column
     private long ownerId;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getActivityId() {
-        return activityId;
-    }
-
-    public void setActivityId(String activityId) {
-        this.activityId = activityId;
-    }
-
-    public Date getScheduledDate() {
-        return scheduledDate;
-    }
-
-    public void setScheduledDate(Date scheduledDate) {
-        this.scheduledDate = scheduledDate;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    public long getOwnerId() {
-        return ownerId;
-    }
-
-    public void setOwnerId(long ownerId) {
-        this.ownerId = ownerId;
-    }
 }
