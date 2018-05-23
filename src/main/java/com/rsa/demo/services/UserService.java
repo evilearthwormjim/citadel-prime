97f6d640f94248f7cf5d7e21a7376a4ef4a82a19
Peter O'Brien <peter.obrien1@rsa.com>
2018-05-23 14:36:16 -0400

    USer find all

--- src/main/java/com/rsa/demo/services/UserService.java
+++ src/main/java/com/rsa/demo/services/UserService.java
@@ -7,11 +7,26 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    public List<UserModel> getAllUsers(){
+        List<UserModel> userList = new ArrayList<>();
+        for(UserEntity entity : userRepository.findAll()){
+
+           UserModel model = new UserModel();
+           BeanUtils.copyProperties(entity, model);
+
+           userList.add(model);
+        }
+        return userList;
+    }
 
     public void saveUser(UserModel user){
 
