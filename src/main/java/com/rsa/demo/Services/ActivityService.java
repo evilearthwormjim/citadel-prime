dd027e8ba8da33b6990b5064e2ee45d33875a399
Reuben Fisher <reuben.fisher@rsa.com>
2018-05-23 12:48:46 -0400

    Change id to a UUID

--- src/main/java/com/rsa/demo/Services/ActivityService.java
+++ src/main/java/com/rsa/demo/Services/ActivityService.java
@@ -4,15 +4,18 @@ import com.rsa.demo.entities.ActivityEntity;
 import com.rsa.demo.models.ActivityModel;
 import com.rsa.demo.repositories.ActivityRepository;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class ActivityService {
 
-    @Autowired
+//    @Autowired - prefer constructor injection as per Dan's guidelines, slide 37
     ActivityRepository repository;
+
+    public ActivityService( ActivityRepository repository) {
+         this.repository = repository;
+    }
 
     public void save(ActivityModel activityMdl){
         ActivityEntity activityEnt = new ActivityEntity();
--- src/main/java/com/rsa/demo/entities/ActivityEntity.java
+++ src/main/java/com/rsa/demo/entities/ActivityEntity.java
@@ -1,50 +1,36 @@
 package com.rsa.demo.entities;
 
 
+import lombok.Setter;
+import lombok.Getter;
+
 import javax.persistence.*;
+import java.util.UUID;
 
 
 @Entity
 public class ActivityEntity {
 
+    // Use a GUID rather than an ID
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private UUID id;
 
+    @Getter
+    @Setter
     @Column
     private String name;
 
+    @Getter @Setter
     @Column
     private String description;
 
+    @Getter @Setter
     @Column
     private boolean active;
 
-    public long getId() {
+    public UUID getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
     }
 }
